2bd211ad954cebd3b2c07b83e33e38cc6e1558fb
Tabish <taabishm2@gmail.com>
2021-06-22 19:39:16 +0530

    Correct javadoc comment

--- src/main/java/com/increff/commons/sql/DbPoolUtil.java
+++ src/main/java/com/increff/commons/sql/DbPoolUtil.java
@@ -18,24 +18,22 @@ import org.apache.commons.dbcp2.BasicDataSource;
 
 public class DbPoolUtil {
 
-    /**
-     * In general, 0 less than minIdle less than maxIdle less than maxTotal
-     * Anything between maxIdle and maxTotal gets deleted automatically
-     * To delete connections between minIdle and maxIdle, there are multiple eviction configuration
-     * We want to avoid that confusion and just have a min and max connection
-     *
-     * Hence for common util, we have following guidelines
-     * 1. MinIdle should not be set. It's default value is zero.
-     * 2. No need to set testWhileIdle & evictionThread properties because it works on MinIdle value.
-     * 3. MaxIdle is the number of connections that will always remain in the pool.
-     * 4. MaxTotal is the number of connections that can be created momentarily, but when connection is used,
-     * total number of connections in the pool becomes equal to MaxIdle value.
-     * 5. Validation Query runs with each and every query to ensure connection is not broken.
-     * NOTE: BasicDataSource Object passed in parameter is expected to only have JDBC driver, username, password, URL.
-     * Example:
-     * For heavy apps-  minConnection(MaxIdle, initialSize) = 30, maxConnection(MaxTotal) = 50
-     * For light apps-  minConnection(MaxIdle, initialSize) = 10, maxConnection(MaxTotal) = 20
-     */
+//    In general, 0 less than minIdle less than maxIdle less than maxTotal
+//    Anything between maxIdle and maxTotal gets deleted automatically
+//    To delete connections between minIdle and maxIdle, there are multiple eviction configuration
+//    We want to avoid that confusion and just have a min and max connection
+//
+//    Hence for common util, we have following guidelines
+//     1. MinIdle should not be set. It's default value is zero.
+//     2. No need to set testWhileIdle & evictionThread properties because it works on MinIdle value.
+//     3. MaxIdle is the number of connections that will always remain in the pool.
+//     4. MaxTotal is the number of connections that can be created momentarily, but when connection is used,
+//    total number of connections in the pool becomes equal to MaxIdle value.
+//     5. Validation Query runs with each and every query to ensure connection is not broken.
+//    NOTE: BasicDataSource Object passed in parameter is expected to only have JDBC driver, username, password, URL.
+//    Example:
+//    For heavy apps-  minConnection(MaxIdle, initialSize) = 30, maxConnection(MaxTotal) = 50
+//    For light apps-  minConnection(MaxIdle, initialSize) = 10, maxConnection(MaxTotal) = 20
     public static BasicDataSource initDataSource(String driverClassName, String url, String userName, String password,
                                        int minConnection, int maxConnection) {
         BasicDataSource bean = new BasicDataSource();
